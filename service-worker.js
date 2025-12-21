dde0d9343f2e5db0705626060f23b7a69d4f5482
MaayanMaxic <163635930+MaayanMaxic@users.noreply.github.com>
2025-12-21 16:34:55 -0500

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "ella-big-trip-v1";
+const ASSETS = [
+  "/Financial_Travel_App/",
+  "/Financial_Travel_App/index.html",
+  "/Financial_Travel_App/styles.css",
+  "/Financial_Travel_App/data.js",
+  "/Financial_Travel_App/db.js",
+  "/Financial_Travel_App/fx.js",
+  "/Financial_Travel_App/app.js",
+  "/Financial_Travel_App/manifest.json",
+  "/Financial_Travel_App/icons/icon-192.png",
+  "/Financial_Travel_App/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
